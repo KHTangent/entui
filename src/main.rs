39d2d141077fb82c0f8cb6c2ea001d5d1f0e6767
KHTangent <contact@khtangent.com>
2026-03-01 12:48:07 +0100

    Add dummy search bar

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use color_eyre::Result;
 use color_eyre::eyre::Context;
 use ratatui::crossterm::event::{self, KeyCode};
 use ratatui::layout::{Constraint, Layout, Margin};
-use ratatui::widgets::{Block, Borders};
+use ratatui::widgets::{Block, Borders, Paragraph};
 use ratatui::{DefaultTerminal, Frame};
 
 use crate::components::departure_list::DepartureList;
@@ -27,11 +27,29 @@ fn run(terminal: &mut DefaultTerminal) -> Result<()> {
 	Ok(())
 }
 fn render(frame: &mut Frame) {
-	let main_layout = Layout::horizontal([Constraint::Fill(1), Constraint::Fill(1)]);
-	let [departure_board, _details] = frame.area().layout(&main_layout);
-	let block = Block::default().borders(Borders::ALL);
-	frame.render_widget(block, departure_board);
-	let departure_board = departure_board.inner(Margin::new(1, 1));
+	let [main_layout_rect, search_bar_rect] = frame.area().layout(&Layout::vertical([
+		Constraint::Fill(1),
+		Constraint::Length(5),
+	]));
+	let [departures_rect, details_rect] = main_layout_rect.layout(&Layout::horizontal([
+		Constraint::Fill(1),
+		Constraint::Fill(1),
+	]));
+
+	let search_bar_block = Block::default()
+		.borders(Borders::ALL)
+		.title_bottom("Stop name");
+	frame.render_widget(search_bar_block, search_bar_rect);
+	let departure_board_block = Block::default().borders(Borders::ALL);
+	frame.render_widget(departure_board_block, departures_rect);
+	let details_block = Block::default().borders(Borders::ALL);
+	frame.render_widget(details_block, details_rect);
+
+	let search_inner = search_bar_rect.inner(Margin::new(2, 2));
+	let dummy_search_text = Paragraph::new("Siemens");
+	frame.render_widget(dummy_search_text, search_inner);
+
+	let departure_board = departures_rect.inner(Margin::new(1, 1));
 	let departures = get_departures("Siemens");
 	frame.render_widget(DepartureList::from(&departures), departure_board);
 }
